2a1612a4cd0151a6add48480ff0ce2f218e4ce7e
MounikaMadishetti <madishettimounika123@gmail.com>
2023-03-22 11:25:47 +0530

    wip

---
+++ SwiftLib11/SwiftLib11.h
@@ -0,0 +1,18 @@
+//
+//  SwiftLib11.h
+//  SwiftLib11
+//
+//  Created by Mounika Madishetti on 22/03/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftLib11.
+FOUNDATION_EXPORT double SwiftLib11VersionNumber;
+
+//! Project version string for SwiftLib11.
+FOUNDATION_EXPORT const unsigned char SwiftLib11VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftLib11/PublicHeader.h>
+
+
---
+++ SwiftLib11Example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftLib11Example
+//
+//  Created by Mounika Madishetti on 22/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
